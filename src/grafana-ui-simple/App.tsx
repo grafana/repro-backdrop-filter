4d5b0d69cfea3a7441ba55c3591b45f2da6ce7f5
joshhunt <josh.hunt@grafana.com>
2025-03-18 12:25:51 +0000

    initial commit

---
+++ src/grafana-ui-simple/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyles } from "@grafana/ui";
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyles />
+
+      <div>Hello World</div>
+    </>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        grafanaUiSimple: resolve(__dirname, "grafana-ui-simple.html"),
+      },
+    },
+  },
+});
